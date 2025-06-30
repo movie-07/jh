b6f0a701ed07c9e4cdffd0e86695baa8289fa2b2
movie-07 <moviehostmain@gmail.com>
2025-06-30 14:32:32 +0530

    Update ContentCard.js

--- components/ContentCard.js
+++ components/ContentCard.js
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button';
 export default function ContentCard({ data, onDelete, onEdit }) {
   return (
     <div className="bg-white p-4 rounded-xl shadow space-y-2" >
-      {<img src={data.img} className="w-full h-48 object-cover rounded" /> }
+      <img src={data.img} className="w-full h-48 object-cover rounded" /> 
       <h2 className="text-lg font-bold">{data.name}</h2>
       <p>⭐ {data.imdbRating} | 🎬 {data.type}</p>
       {/* <p className="text-sm text-gray-600">{data.description.slice(0, 80)}...</p> */}
