a5a0774d2c8cb66c9dbd01a22caa70ccf331b65e
xlarabout <113415294+xlarabout@users.noreply.github.com>
2025-07-02 03:45:51 +0530

    ggg
    ggg

---
+++ models/Content.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+import slugify from 'slugify'; // make sure to install it
+
+const contentSchema = new mongoose.Schema({
+  name: String,
+  slug: { type: String, unique: true },
+  img: String,
+  img2: String,
+  imdbRating: String,
+  description: String,
+  imdbLink: String,
+  genre: [String],
+  type: { type: String, enum: ['movie', 'webseries'], required: true },
+  downloadLinks: [
+    {
+      name: String,
+      url: String,
+    }
+  ],
+  createdAt: { type: Date, default: Date.now },
+});
+
+// Auto-generate slug before saving hello niswa
+contentSchema.pre('save', function (next) {
+  if (!this.slug && this.name) {
+    this.slug = slugify(this.name, { lower: true, strict: true });
+  }
+  next();
+});
+
+export default mongoose.models.Content || mongoose.model('Content', contentSchema);
